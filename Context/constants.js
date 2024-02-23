7e5cf2b2b7815aad3e2f95a47046fe5bac05e533
Artem Popov <121538758+highxshell@users.noreply.github.com>
2024-02-23 14:19:26 +0600

    Add files via upload

---
+++ Context/constants.js
@@ -0,0 +1,11 @@
+import ERC20Generator from "./ERC20Generator.json";
+import LookUpContract from "./LookUpContract.json";
+
+export const ERC20Generator_ABI = ERC20Generator.abi;
+export const ERC20Generator_BYTECODE = ERC20Generator.bytecode;
+export const ERC20Generator_ADDRESS =
+  "0x21A0513946A5f33273aBF24120Ce7ACFe9Ab9af6";
+
+export const LookUpContract_ABI = LookUpContract.abi;
+export const LookUpContract_ADDRESS =
+  "0xc5bcD85cA56B4F789CE5E54b480E0463AB3C79fB";
